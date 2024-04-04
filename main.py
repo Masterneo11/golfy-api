2d540cc3fdfc966d8ca9b67280f74c1a8765ab27
Mathew Zamacona <mateoszamacona@icloud.com>
2024-04-04 10:39:51 -0600

    Add endpoints

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
+import uuid
+
 from fastapi import FastAPI
 
-from models import Player, Course
+from models.course import Course
+from models.player import Player
+
+
 
 app = FastAPI()
+
+
+players: dict[uuid.UUID, Player] = {}
+courses: dict[uuid.UUID, Course] = {}
+
+@app.get("/players")
+async def get_player():
+    pass
+
+@app.post("/players")
+async def new_player():
+    pass
+
+@app.delete("/players{player_id}")
+async def del_player(player_id):
+    pass
+
+@app.put("/players{player_id}")
+async def update_player(player_id):
+    pass
+
+@app.get("/courses")
+async def get_course():
+    pass
+
+@app.post("/courses")
+async def new_course():
+    pass
+
+@app.put("/courses{course_id}")
+async def update_course(course_id):
+    pass
+
+@app.delete("/courses{course_id}")
+async def del_course(course_id):
+    pass
